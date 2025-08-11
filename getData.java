ce4216ef176179ed28276e2754edd9114ce818cf
KarpagalakshmiK <karpagalakshmik70@gmail.com>
2025-08-11 11:30:42 +0530

    Update getData.java

--- getData.java
+++ getData.java
@@ -6,16 +6,17 @@ import java.util.ArrayList;
 public class getData {
 	
 	public static void main(String args[]) throws IOException {
-//		ExcelDriven ex=new ExcelDriven();
-//		ArrayList l=ex.getData("Her Innocent Love");
-//		
-//	System.out.println(l.get(0));
-//	System.out.println(l.get(1));
-//	System.out.println(l.get(2));
-//	System.out.println(l.get(3));
-//	System.out.println(l.get(4));
+		ExcelDriven ex=new ExcelDriven();
+		ArrayList l=ex.getData("Her Innocent Love");
+		
+	System.out.println(l.get(0));
+	System.out.println(l.get(1));
+	System.out.println(l.get(2));
+	System.out.println(l.get(3));
+	System.out.println(l.get(4));
 	
 		
 	}
 
 }
+
